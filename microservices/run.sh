c70a88ed377c67acc6d8a3e2abd1fac04fb69ca9
iustinadumitru <iustinadumitru21@gmail.com>
2019-05-04 15:12:20 +0300

    enabled fs and processes in run.sh

--- microservices/run.sh
+++ microservices/run.sh
@@ -1,10 +1,10 @@
 npm install
 node app.js stats       8001 &
 # node app.js users       8002 &
-# node app.js filesystem  8003 &
+node app.js filesystem  8003 &
 # node app.js logs        8004 &
 # node app.js console     8005 &
 # node app.js configs     8006 &
-# node app.js processes   8007 &
+node app.js processes   8007 &
 
 node app.js test        8100 &
