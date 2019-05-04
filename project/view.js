f562657f5f77ec5e27677fde5317139b3b6db670
David VULTUR <dvultur@bitdefender.com>
2019-05-04 17:35:39 +0300

    Added cookies in interface

--- project/view.js
+++ project/view.js
@@ -2,9 +2,16 @@ var fs = require('fs')
 var mustache = require('mustache')
 
 var Template = function(path = './templates/') {
-    var render = function(res, tpl_name, vars = []) {
+    var render = function(res, tpl_name, vars = [], cookies = {}) {
+        var head_lines = {'Content-Type': 'text/html'}
 
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        if(cookies){
+            head_lines['Set-Cookie'] = ''
+            for(var key in cookies){
+                head_lines['Set-Cookie'] = `${encodeURI(key)}=${encodeURI(cookies[key])}`
+            }
+        }
+        res.writeHead(200, head_lines);
 
         fs.readFile(path + tpl_name, 'utf-8', function(err, data){
             if(err) return console.log("Error reading:", err.message)
